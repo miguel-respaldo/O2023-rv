eadd9bfb315d113443121e4bc466badb529db25b
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2023-11-04 09:35:12 -0600

    add jugador

---
+++ jugador.gd
@@ -0,0 +1,41 @@
+extends RigidBody3D
+
+var mouse_sensibilidad := 0.001
+var izder_mov := 0.0
+var arrab_mov := 0.0
+
+@onready var giro        := $izq_derecha
+@onready var inclinacion := $izq_derecha/arriba_abajo
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#var input = Input.get_action_strength("ui_up")
+	#apply_central_force(input * Vector3.FORWARD *  1200.0 * delta)
+	var input = Vector3.ZERO
+	input.x = Input.get_axis("mover_izquierda","mover_derecha")
+	input.z = Input.get_axis("mover_adelante", "mover_atras")
+	apply_central_force(giro.basis * input * 1200.0 * delta)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	giro.rotate_y(izder_mov)
+	inclinacion.rotate_x(arrab_mov)
+	inclinacion.rotation.x = clamp(
+		inclinacion.rotation.x,
+		deg_to_rad(-30),  # -0.5
+		deg_to_rad(30)    #  0.5
+	)
+	izder_mov = 0.0
+	arrab_mov = 0.0
+
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			izder_mov = - event.relative.x * mouse_sensibilidad
+			arrab_mov = - event.relative.y * mouse_sensibilidad
